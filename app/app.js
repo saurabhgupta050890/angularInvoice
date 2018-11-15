da6f7f07ca21e48730632f91200cd3dd0fcdfcd3
Saurabh Gupta <saurabhgupta0508@gmail.com>
2018-11-15 22:35:53 +0530

    Fixed Parser

--- app/app.js
+++ app/app.js
@@ -1,21 +1,19 @@
-angular.module('InvoiceParser', [])
-    .controller('AppCtrl', AppCtrl);
+angular.module("InvoiceParser", []).controller("AppCtrl", AppCtrl);
 
-AppCtrl.$inject = ['$scope', 'ParcerService'];
+AppCtrl.$inject = ["$scope", "ParcerService"];
 
 function AppCtrl($scope, ParcerService) {
-    $scope.onFileSelect = function($fileContent) {
-        $scope.invoice = $fileContent;
+  $scope.onFileSelect = function($fileContent) {
+    $scope.invoice = $fileContent;
 
-        var nums = $fileContent.split("\n\n");
-        var parsedNums = [];
-        console.log(nums.length);
-        nums.forEach(function(number) {
-            if(number) {
-                parsedNums.push(ParcerService.parseSevenSegmentASCII(number));
-            }
-        });
-        
-        //console.log(parsedNums);
-    }
+    var nums = $fileContent.split("\n\n");
+    var parsedNums = [];
+    nums.forEach(function(number) {
+      if (number) {
+        parsedNums.push(ParcerService.parseSevenSegmentASCII(number));
+      }
+    });
+    $scope.parsed = parsedNums.join("\n");
+    //console.log(parsedNums);
+  };
 }
--- app/parser.service.js
+++ app/parser.service.js
@@ -1,52 +1,54 @@
-angular.module('InvoiceParser')
-    .service('ParcerService', ParcerService)
+angular.module("InvoiceParser").service("ParcerService", ParcerService);
 
 ParcerService.$inject = [];
 
-
 function ParcerService() {
-    var bitMap = {
-        63: 0,
-        6: 1,
-        91: 2,
-        79: 3,
-        102: 4,
-        109: 5,
-        125: 6,
-        7: 7,
-        127: 8,
-        111: 9,
-        0: " ",
-        119: "A",
-        124: "b",
-        57: "C",
-        62: "d",
-        121: "E",
-        113: "F"
-    };
+  let bitMap = {
+    63: 0,
+    6: 1,
+    91: 2,
+    79: 3,
+    102: 4,
+    109: 5,
+    125: 6,
+    7: 7,
+    127: 8,
+    111: 9,
+    0: " ",
+    119: "A",
+    124: "b",
+    57: "C",
+    62: "d",
+    121: "E",
+    113: "F"
+  };
 
-    this.parseSevenSegmentASCII = function(ascii) {
-        console.log(ascii.split("\n"));
-        return ascii
-            .split("\n")
-            .reduce(function(arr, part, index) {
-                //Split part into group of 3
-                part.match(/.../g).forEach(function(b, j) {
-                    arr[j] = arr[j] || [];
-                    arr[j][index] = b;
-                });
-                return arr;
-            }, [])
-            .map(function(a) {
-                //console.log(a);
-                return a.join("");
-            })
-            .map(function(num) {
-                var result = "909561432".split("").reduce(function(res, val, i) {
-                    return res + num[i] !== " " ? Math.pow(2, parseInt(val, 10)) : 0;
-                }, 0);
-                return bitMap[result] || "?"; //? is the invalid character
-            })
-            .join("");
-    }
+  this.parseSevenSegmentASCII = function(ascii) {
+    return ascii
+      .split("\n")
+      .reduce(decodeSegmentReducer, [])
+      .map(a => a.join(""))
+      .map(num => {
+        var result = getNumberFromBitmap(num);
+        return bitMap[result].toString() || "?"; //? is the invalid character
+      })
+      .join("");
+  };
+
+  let decodeSegmentReducer = (arr, part, index) => {
+    part.match(/.../g).forEach((b, j) => {
+      arr[j] = arr[j] || [];
+      arr[j][index] = b;
+    });
+    return arr;
+  };
+
+  let getNumberFromBitmap = number =>
+    "909561432"
+      .split("")
+      .reduce(
+        (res, val, i) =>
+          res + (number[i] !== " " ? Math.pow(2, parseInt(val, 10)) : 0),
+        0
+      );
 }
